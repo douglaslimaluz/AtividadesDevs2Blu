5c850c12fb6783fc1d2a4594ae2949c5285a2855
douglaslimaluz <60515470+douglaslimaluz@users.noreply.github.com>
2021-11-29 21:47:54 -0300

    Add files via upload

---
+++ AtividadesIniciais/UsoSwitch.java
@@ -0,0 +1,40 @@
+import javax.swing.JOptionPane;
+
+public class UsoSwitch {
+
+	public static void main(String[] args) {
+		
+		//
+		
+		int opcao;
+		
+		opcao = Integer.parseInt(JOptionPane.showInputDialog(" Informe a opção "));
+				
+	           switch (opcao) {
+	           case 1:{
+	        	   System.out.println("INCLUSÃO");
+	        	   
+	           }
+	           
+	           case 2: {
+	        	   System.out.println("ALTERAÇÃO");
+	        	 
+	           
+	             
+	           
+	        	  
+	           }
+	          
+	           
+	        	   
+	           }
+	        
+	        
+	
+
+	    
+	    
+	    
+	    
+	    
+}
